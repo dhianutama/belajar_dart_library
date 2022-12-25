736014611a626a440ecbc25658f5df13956e4b51
Dhian Utama <dhian.utama@gmail.com>
2022-12-25 15:32:52 +0700

    dart library

---
+++ example/hello.dart
@@ -0,0 +1,11 @@
+import 'package:belajar_dart_library/library.dart';
+
+void main(){
+  print(sayHello("Dhian"));
+
+  var customer = Customer();
+  var product = Product();
+  var category = Category();
+
+}
+
